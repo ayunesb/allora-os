69f13453fd1f4f75ccbabcbe4ae908e0c7bca6cf
ayunesb <ayunesb@icloud.com>
2025-04-30 09:42:34 -0500

    c

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
-  plugins: [
-    react(),
-    mode === 'development' && componentTagger(),
-  ].filter(Boolean),
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -16,6 +11,6 @@ export default defineConfig(({ mode }) => ({
   },
   server: {
     port: 8080,
-    host: "::",
+    host: '::',
   },
-}));
+});
