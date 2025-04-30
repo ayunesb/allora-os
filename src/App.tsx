558a624773e1d0be897b215f1dfda9893d758a69
ayunesb <ayunesb@icloud.com>
2025-04-30 15:26:49 -0500

    fix: remove invalid children prop from GlobalErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const App = () => {
   }, []);
 
   return (
-    <GlobalErrorBoundary children={undefined}>
+    <GlobalErrorBoundary>
       <ThemeProvider>
         <BrowserRouter>
           <AuthProvider>
